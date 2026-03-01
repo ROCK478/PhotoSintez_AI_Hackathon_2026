20e9afb09ea6c23c9068cea40447753d54638e60
Виктор <nikola.tesla.1973@mail.ru>
2026-03-01 12:42:29 +0500

    .

--- app.py
+++ app.py
@@ -59,4 +59,6 @@ def get_result(filename):
     return send_from_directory(RESULT_FOLDER, filename)
 
 if __name__ == "__main__":
-    app.run(debug=True, port=5000)
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
